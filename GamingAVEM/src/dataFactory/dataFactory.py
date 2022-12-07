eb1f112c294c1cbc0b26957fd7e216c93320b5ea
Mad <ted15945@gmail.com>
2022-12-07 21:37:20 +0800

    fix dataFactory

--- GamingAVEM/src/dataFactory/dataFactory.py
+++ GamingAVEM/src/dataFactory/dataFactory.py
@@ -1,13 +1,15 @@
 import sys
 sys.path.append('./')
 
-from src.dataPreprocess.preprocessVisual import *
+from src.dataPreprocess.preprocessVisual import capture_visual_rgb_npys as rgb
+from src.dataPreprocess.preprocessVisual import capture_visual_npys as npy
+from src.dataPreprocess.preprocessVisual import capture_visual_au_npys as au
 from src.common.utilities import * 
 
 def capture_visual_rgb_npys(input_path=ENTERFACE_DATA_PATH, output_path=ENTERFACE_VISUAL_NPY_DATA_PATH, class_labels=ENTERFACE_LABELS, time_F=1, resize_x=224, resize_y=224):
     timepoint_start, _ = get_timepoint()
 
-    capture_visual_rgb_npys(input_path=input_path, output_path=output_path, class_labels=class_labels)
+    rgb(input_path=input_path, output_path=output_path, class_labels=class_labels)
 
     timepoint_end, _ = get_timepoint()
     _, time_cost_msg = get_time_cost(timepoint_start, timepoint_end)    
@@ -17,7 +19,7 @@ def capture_visual_npys(input_path=ENTERFACE_DATA_PATH, output_path=ENTERFACE_VI
                         resize_x=224, resize_y=224, to_local_binary_pattern=False, to_interlaced_derivative_pattern=False, selector='',is_reverse=False , key_frames_count=0):
     timepoint_start, _ = get_timepoint()
 
-    capture_visual_npys(input_path=input_path, output_path=output_path, class_labels=class_labels, selector=selector)
+    npy(input_path=input_path, output_path=output_path, class_labels=class_labels, selector=selector)
 
     timepoint_end, _ = get_timepoint()
     _, time_cost_msg = get_time_cost(timepoint_start, timepoint_end)    
@@ -26,7 +28,7 @@ def capture_visual_npys(input_path=ENTERFACE_DATA_PATH, output_path=ENTERFACE_VI
 def capture_visual_au_npys(input_path=ENTERFACE_DATA_PATH, output_path=ENTERFACE_VISUAL_AU_DATA_PATH):
     timepoint_start, _ = get_timepoint()
 
-    capture_visual_au_npys(input_path=input_path, output_path=output_path)
+    au(input_path=input_path, output_path=output_path)
 
     timepoint_end, _ = get_timepoint()
     _, time_cost_msg = get_time_cost(timepoint_start, timepoint_end)    
